9e1111be17b5a110525eeeb967cb4344d3151a38
Williams Caballero Araya <knightex22@gmail.com>
2021-12-23 15:37:50 -0300

    Nodificar paginas y agregar logica para generacion del password
    Agregar bootstrap para estilos del sitio

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import about, home
+from .views import about, home, password
 
 urlpatterns = [
-    path('',home),
-    path('about/',about),
+    path('',home,name="home"),
+    path('about/',about,name="about"),
+    path('password/',password, name="password"),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
+import random
 
 def home(request):
     return render(request,"core/home.html")
+
+
+def password(request):
+
+    password = ""
+
+    # TODO --> Crear una lista de las letras del abecedario
+    characters = list("abcdefghijklmnopqrstuvwxyz")
+
+    # TODO --> Obtener el largo de la contraseña desde el formulario de home y guardarlo en una variable
+    length = int(request.GET.get('length'))
+
+    # TODO --> Obtener el valor del checkbox para mayusculas
+    uppercase = request.GET.get('uppercase')
+
+    if uppercase:
+        characters.extend(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+    
+    if request.GET.get('special'):
+        characters.extend(list("@#$%&*+-_!?"))
+
+    if request.GET.get('numbers'):
+        characters.extend(list("1234567890"))
+
+
+    # Recorrer la lista
+    for i in range(length):
+
+        password += random.choice(characters)
+            
+
+    return render(request,"core/password.html", {"password":password})
+
+
 
 def about(request):
     return render(request,"core/about.html")
